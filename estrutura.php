2aa74b19a19439c96c4e5350ee04baa3d6927158
martimvalente <56270192+martimvalente@users.noreply.github.com>
2020-07-16 12:53:32 +0100

    mudancas

--- estrutura.php
+++ estrutura.php
@@ -12,52 +12,62 @@
         <!-- Conexão da navbar -->
 	      <?php include('navbar_dashboard.php'); ?>
 
-        <!-- <center style=" margin-top:25px;"><h1>Título da página</h1></center>   // Título Extra-->
+         <center style=" margin-top:25px;"><h1>Título da página</h1></center>
 
         <!-- Conteúdo da página -->
         <div class="card" style=" margin-top:25px;">
 
             <!-- Titulo + Botões  -->
             <div class="card-header">
-                <h3 class="panel-title">Qualquer Coisa</h3>
+                <h3 class="panel-title">Titulo ou Subtítulo</h3>
             </div>
 
             <!-- Tabelas / Forms / TUDO -->
             <div class="card-body">
 
-                <h5 class="card-title">Conteudo aqui</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-default">Clicar wtf</a>
-
-
+                <h5 class="card-title">Exemplo Texto</h5>
+                <p class="card-text">I don't hate Chris Martin. I don`t know him, know what I mean? I just thinks he`s a bit giddy. He ought to calm down, he isn`t gonna save the world.</p>
+                <p class="card-text">The Beatles play guitars, we play guitars. The Beatles got hair, we've got hair. The Beatles got arms, we've got arms.</p>
+                <hr>
+                <h5 class="card-title">Exemplo Botões</h5>
+                <a href="#" class="btn btn-default">Botão1</a>
+                <button type="button" class="btn btn-success">Sucesso</button>
+                <button type="button" class="btn btn-danger">Erro</button>
+                <button type="button" class="btn btn-warning">Aviso</button>
+                <hr>
                 <h5 class="card-title">Exemplo tabela</h5>
                 <table class="table table-bordered">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Tab1</th>
+                        <th scope="col">Tab2</th>
+                        <th scope="col">Tab3</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                         <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>Two Door Cinema Club</td>
+                        <td>Fatboy Slim </td>
+                        <td>Robbie Williams</td>
                         </tr>
                         <tr>
                         <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>Guy Chambers</td>
+                        <td>The Cure</td>
+                        <td>Hiltop Hoods</td>
                         </tr>
                         <tr>
                         <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>Kasabian</td>
+                        <td>Liam Gallagher</td>
+                        <td>Steely Dan</td>
+                        <tr>
+                        <th scope="row">4</th>
+                        <td>Elton John</td>
+                        <td>Muse</td>
+                        <td>Royal Blood</td>
                         </tr>
                     </tbody>
                 </table>
